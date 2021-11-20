f914191ecfaeefafae3ff36e79a4b84f0c961430
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-20 19:21:09 +0700

    test

--- app.py
+++ app.py
@@ -49,14 +49,12 @@ def callback():
 
 
 def reply(intent,text,reply_token,id,disname):
-    text_message = TextSendMessage(text='ในช่วง 5 วันที่ผ่านมาคุณมีค่าออกซึเจนในเลือดต่ำกว่า 90 % ควรไปพบแพทย์เพื่อตรวจเช็คสุขภาพ')
-    line_bot_api.reply_message(reply_token,text_message)
     if intent == 'Healthcare':
         a = callAPI()
         text_message = TextSendMessage(text='Heartrate : ' + str(a['Heartrate_O2']['H']) + ' bpm' + ' O2 : ' + str(a['Heartrate_O2']['O2']) + ' %')
         line_bot_api.reply_message(reply_token,text_message)
     if intent == 'test':
-        text_message = TextSendMessage(text='')
+        text_message = TextSendMessage(text='X : ' + str(a['Heartrate_O2']['X']) + ' m/s2' + ' y : ' + str(a['Heartrate_O2']['y']) + ' m/s2')
         line_bot_api.reply_message(reply_token,text_message)
 
 if __name__ == "__main__":
