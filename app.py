b5951835004be2e214eaea84ac98d21df2ffacb5
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-12-20 17:02:58 +0700

    testy

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ def callback():
     intent = req["queryResult"]["intent"]["displayName"]
     entity = req["queryResult"]["parameters"]["bodytypeCheck"]
     print(entity)
-    #text = req['originalDetectIntentRequest']['payload']['data']['message']['text']
-    #reply_token = req['originalDetectIntentRequest']['payload']['data']['replyToken']
-    #id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
-    #disname = line_bot_api.get_profile(id).display_name
+    text = req['originalDetectIntentRequest']['payload']['data']['message']['text']
+    reply_token = req['originalDetectIntentRequest']['payload']['data']['replyToken']
+    id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
+    disname = line_bot_api.get_profile(id).display_name
     '''
     print('id = ' + id)
     print('name = ' + disname)
